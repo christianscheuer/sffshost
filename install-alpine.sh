5688ad25ebca1f106445e6cf79d34308855f382a
Christian Scheuer <info@creatix.dk>
2019-06-11 01:37:46 +0200

    More fixes to install-alpine.sh

--- install-alpine.sh
+++ install-alpine.sh
@@ -1,7 +1,7 @@
 # Install Docker
 apk add docker
 apk update
-rc-update add docker boot
+rc-update add docker #works better to add at default runlevel, not boot!
 service docker start
 service docker status
 docker -v
@@ -14,6 +14,32 @@ pip install docker-compose
 
 # Install curl (for testing purposes)
 apk add curl
+
+# Now patch inittab
+echo '
+# /etc/inittab
+
+::sysinit:/usr/bin/clear
+::sysinit:/bin/echo "Starting SoundFlow Facility Server..."
+::sysinit:/sbin/openrc sysinit --quiet &>/dev/null
+::sysinit:/sbin/openrc boot --quiet &>/dev/null
+::wait:/sbin/openrc default --quiet &>/dev/null
+
+# Set up a couple of gettys
+tty1::respawn:/usr/bin/nano /etc/network/interfaces
+tty2::respawn:/sbin/getty 38400 tty2
+
+# Stuff to do for the 3-finger salute
+::ctrlaltdel:/sbin/reboot
+
+# Stuff to do before rebooting
+::shutdown:/usr/bin/clear
+::shutdown:/sbin/openrc shutdown
+
+' > /etc/inittab
+
+# Now patch /boot/extlinux.conf
+cat /boot/extlinux.conf | sed 's/AUTOBOOT Alpine/AUTOBOOT SFFS/' | sed 's/\(APPEND .*\)/\1 quiet loglevel=1 /' > /boot/extlinux.conf
 
 # Make sure we're in the right directory
 cd /opt/sffs
@@ -51,3 +77,6 @@ echo "in pkg/sffs: jake send, enter IP: $SFFSIP, use password sffshost"
 
 echo ""
 echo "When done transferring, do this in VM: cd /opt/sffs && ./start.sh"
+
+# Notes for networking:
+# https://wiki.alpinelinux.org/wiki/Configure_Networking
